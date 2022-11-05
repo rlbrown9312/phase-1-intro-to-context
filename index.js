c19d219060c1eeb891bbdb2d97812feddee93225
Richard <richard.brown0807@gmail.com>
2022-11-04 20:33:57 -0500

    commit tree f9ac2d03cadfc703083b85e86606586075c2a515

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+const emp = ["Gray", "Worm", "Security", 1]
+
+ const [firstName, familyName, title, payPerHour] = emp
+
+ //console.log(firstName)
+// console.log(first)
+
+//const ["Gray": firstName] = emp
+
+// const asgardianBrothers = [
+//     {
+//       firstName: "Thor",
+//       familyName: "Odinsson"
+//     },
+//     {
+//       firstName: "Loki",
+//       familyName: "Laufeysson-Odinsson"
+//     }
+//   ]
+
+
+
+  function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return [ title, payPerHour]
+        //return employee
+  }
+console.log(createEmployeeRecord(emp))
+
+
+// class Employees{
+//    createEmployeeRecord(firstName, lastName, title, pay) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.title = title;
+//     this.pay = pay
+
+    
+//     }
+// }
+
+// const [firstName, lastName, title, pay] = createEmployeeRecord()
+
+
+// function createEmployeeRecord() {
+//     return [firstName, familyName, title, payPerHour]
+// }
+
+function createTimeInEvent() {
+
+}
+
+function createTimeOutEvent() {
+
+}
